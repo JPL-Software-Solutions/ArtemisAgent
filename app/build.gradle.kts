9d0c00a331003aaac0b1df40d7c1f21345600d18
Jordan Longstaff <jrdnlngstff@gmail.com>
2026-04-21 08:27:01 -0400

    Switch from named to matching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -120,7 +120,7 @@ androidComponents {
         // As of AGP 9, release test tasks are no longer generated, so we depend on the
         // debug task instead
         tasks
-            .named { it == "assemble${variantName}" }
+            .matching { it.name == "assemble${variantName}" }
             .configureEach { dependsOn(":app:konsist:testDebugUnitTest") }
         tasks
             .named { it.startsWith("ksp$variantName") && it.endsWith("Kotlin") }
