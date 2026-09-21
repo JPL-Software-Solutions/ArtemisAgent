9438ecefe86b05850966118f46fb89adc4989ae7
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2026-09-21 15:11:24 +0000

    Try Emulator.WTF integration (#1035)
    * Try Emulator.WTF integration
    
    * Fix action ref
    
    * App APK required
    
    * Fix model spec, attempt 1
    
    * Re-fix action ref
    
    * Fix timeout
    
    * Clear app data between tests
    
    * API 37 only
    
    * Tweak ConnectScenario to adapt to emulator.wtf
    
    * Shard tests
    
    * Specify test output directory
    
    * Increase timeout
    
    * More accurate fix for connection failure test
    
    * Fix shards argument
    
    * Fix flaky vessel data disconnect test

--- app/src/androidTest/kotlin/artemis/agent/setup/ConnectFragmentTest.kt
+++ app/src/androidTest/kotlin/artemis/agent/setup/ConnectFragmentTest.kt
@@ -154,7 +154,14 @@ class ConnectFragmentTest : TestCase() {
     fun connectionFailedTest() {
         run {
             mainScreenTest {
-                scenario(ConnectScenario("127.0.0.1", activityScenarioRule.scenario, check = null))
+                scenario(
+                    ConnectScenario(
+                        "127.0.0.1",
+                        activityScenarioRule.scenario,
+                        shouldCheckConnection = false,
+                        check = null,
+                    )
+                )
 
                 ConnectPageScreen {
                     step("Failure state") {
